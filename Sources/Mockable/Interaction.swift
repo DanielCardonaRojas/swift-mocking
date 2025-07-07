ca02185a47b69ea5fb4d87592780e2343c1a7f4b
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-07 00:08:48 -0500

    Add interaction type

---
+++ Sources/Mockable/Interaction.swift
@@ -0,0 +1,17 @@
+//
+//  Interaction.swift
+//  Mockable
+//
+//  Created by Daniel Cardona on 6/07/25.
+//
+
+public struct Interaction<each Input, Eff: Effect, Output> {
+    public let invocationMatcher: InvocationMatcher<repeat each Input>
+    public let spy: Spy<repeat each Input, Eff, Output>
+
+    public init(matchers: repeat ArgMatcher<each Input>, spy: Spy<repeat each Input, Eff, Output>) {
+        self.invocationMatcher = InvocationMatcher(matchers: repeat each matchers)
+        self.spy = spy
+    }
+}
+
