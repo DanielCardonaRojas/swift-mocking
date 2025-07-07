45d48bc0875aa4a95dd96f6089c64e5ed24afc7f
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-07 08:12:33 -0500

    InvocationMatcher cleanup

--- Sources/Mockable/InvocationMatcher.swift
+++ Sources/Mockable/InvocationMatcher.swift
@@ -5,14 +5,17 @@
 //  Created by Daniel Cardona on 4/07/25.
 //
 
+/// A type that matches a set of arguments against a set of ``ArgMatcher``s.
+///
+/// This type is used to represent all parameters of a method signature.
 public struct InvocationMatcher<each I> {
-    let matchers: (repeat ArgMatcher<each I>)
+   let matchers: (repeat ArgMatcher<each I>)
 
-    init(matchers: repeat ArgMatcher<each I>) {
+    public init(matchers: repeat ArgMatcher<each I>) {
         self.matchers = (repeat each matchers)
     }
 
-    func isMatchedBy(_ invocation: (repeat each I)) -> Bool {
+    public func isMatchedBy(_ invocation: (repeat each I)) -> Bool {
         func match <each Element>(inputs: (repeat each Element), matchers: (repeat ArgMatcher<each Element>)) -> Bool {
           for (input, matcher) in repeat (each inputs, each matchers) {
             guard matcher(input) else { return false }
