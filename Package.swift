9254ae3a81046058a80c28ab05cdbe6767349669
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-14 23:51:19 -0500

    Remove macCatalyst

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         .iOS(.v17),
         .tvOS(.v13),
         .watchOS(.v10),
-        .macCatalyst(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
