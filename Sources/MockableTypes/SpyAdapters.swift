696fe31372a761028eadcdac3fcf663f6570d506
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-17 15:02:31 -0500

    Add static variants for adapters

--- Sources/MockableTypes/SpyAdapters.swift
+++ Sources/MockableTypes/SpyAdapters.swift
@@ -45,6 +45,8 @@ public func adapt<Context: DefaultProvider, each I, O>(_ keyPath: KeyPath<Contex
     }
 }
 
+// MARK: - Non Keypath variants
+
 /// A helper that converts a spy into a closure that is easily assignable to a protocol witness closure property.
 public func adaptNone<Context: DefaultProvider, each I, O>(_ context: Context, _ spy: Spy<repeat each I, None, O>) -> (Context, repeat each I) -> O {
     { (context, input: repeat each I) -> O  in
@@ -83,3 +85,36 @@ public func adaptAsyncThrows<Context: DefaultProvider, each I, O>(
     }
 }
 
+// MARK: Static variants
+
+/// A helper that converts a spy into a closure that is easily assignable to a protocol witness closure property.
+public func adaptNone<each I, O>(_ spy: Spy<repeat each I, None, O>) -> (repeat each I) -> O {
+    { (input: repeat each I) -> O  in
+        let result = spy.call(repeat each input)
+        return result
+    }
+}
+
+/// A helper that converts a spy into a closure that is easily assignable to a protocol witness closure property.
+public func adaptThrows<each I, O>(_ spy: Spy<repeat each I, Throws, O>) -> (repeat each I) throws -> O {
+    { (input: repeat each I) -> O  in
+        let result = try spy.call(repeat each input)
+        return result
+    }
+}
+
+/// A helper that converts an `async` spy into a closure that is easily assignable to a protocol witness closure property.
+public func adaptAsync<each I, O>(_ spy: Spy<repeat each I, Async, O>) -> (repeat each I) async -> O {
+    { (input: repeat each I) async -> O  in
+        let result = await spy.call(repeat each input)
+        return result
+    }
+}
+public func adaptAsyncThrows<each I, O>(
+    _ spy: Spy<repeat each I, AsyncThrows, O>
+) -> (repeat each I) async throws -> O {
+    { (input: repeat each I) async throws -> O  in
+        let result = try await spy.call(repeat each input)
+        return result
+    }
+}
