8908052bfc5978e4b9cd3644578da58bb621a20a
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-21 00:08:47 -0500

    Add extensions

--- Sources/MockableGenerator/SwiftSyntax+Extensions.swift
+++ Sources/MockableGenerator/SwiftSyntax+Extensions.swift
@@ -11,3 +11,35 @@ extension DeclModifierSyntax {
         name.tokenKind == .keyword(.static)
     }
 }
+
+extension VariableDeclSyntax {
+    var hasGetter: Bool {
+        bindings.first?.accessorBlock?.accessors.hasGetter ?? false
+    }
+    var hasSetter: Bool {
+        bindings.first?.accessorBlock?.accessors.hasSetter ?? false
+    }
+
+    var name: TokenSyntax {
+        bindings.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier ?? "Unknown"
+    }
+}
+
+extension AccessorBlockSyntax.Accessors {
+  var settersAndGetters: AccessorDeclListSyntax? {
+    switch self {
+    case .accessors(let settersAndGetters):
+      return settersAndGetters
+    case .getter(_):
+      return nil
+    }
+  }
+
+  var hasGetter: Bool {
+    settersAndGetters?.first(where:  { $0.accessorSpecifier.text == TokenSyntax.keyword(.get).text }) != nil
+  }
+
+  var hasSetter: Bool {
+    settersAndGetters?.first(where:  { $0.accessorSpecifier.text == TokenSyntax.keyword(.set).text}) != nil
+  }
+}
