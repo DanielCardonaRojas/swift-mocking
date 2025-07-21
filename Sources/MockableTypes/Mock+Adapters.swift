f60737dd3210e525714afca4c40d377c482e59f9
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-20 23:13:48 -0500

    Update adapters

--- Sources/MockableTypes/Mock+Adapters.swift
+++ Sources/MockableTypes/Mock+Adapters.swift
@@ -6,25 +6,25 @@
 //
 
 public extension Mock {
-    static func adapting<each I, O>(_ spy: Spy<repeat each I, None, O>, _ input: repeat each I) -> O {
+    static func adapt<each I, O>(_ spy: Spy<repeat each I, None, O>, _ input: repeat each I) -> O {
         spy.defaultProviderRegistry = defaultProviderRegistry
         let result = spy.call(repeat each input)
         return result
     }
 
-    func adapting<each I, O>(_ spy: Spy<repeat each I, None, O>, _ input: repeat each I) -> O {
+    func adapt<each I, O>(_ spy: Spy<repeat each I, None, O>, _ input: repeat each I) -> O {
         spy.defaultProviderRegistry = defaultProviderRegistry
         let result = spy.call(repeat each input)
         return result
     }
 
-    static func adapting<each I, O>(_ spy: Spy<repeat each I, Async, O>, _ input: repeat each I) async -> O {
+    static func adapt<each I, O>(_ spy: Spy<repeat each I, Async, O>, _ input: repeat each I) async -> O {
         spy.defaultProviderRegistry = defaultProviderRegistry
         let result = await spy.call(repeat each input)
         return result
     }
 
-    func adapting<each I, O>(_ spy: Spy<repeat each I, Async, O>, _ input: repeat each I) async -> O {
+    func adapt<each I, O>(_ spy: Spy<repeat each I, Async, O>, _ input: repeat each I) async -> O {
         spy.defaultProviderRegistry = defaultProviderRegistry
         let result = await spy.call(repeat each input)
         return result
@@ -48,7 +48,7 @@ public extension Mock {
         return result
     }
 
-    public func adaptThrowing<each I, O>(_ spy: Spy<repeat each I, AsyncThrows, O>, _ input: repeat each I) async throws -> O {
+    func adaptThrowing<each I, O>(_ spy: Spy<repeat each I, AsyncThrows, O>, _ input: repeat each I) async throws -> O {
         spy.defaultProviderRegistry = defaultProviderRegistry
         let result = try await spy.call(repeat each input)
         return result
