50a5db179d26c7d6480b335961b47171c1d9781f
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-07-29 12:24:15 -0500

    Add Assert tests

---
+++ Tests/SwiftMockingTests/AssertTests.swift
@@ -0,0 +1,93 @@
+
+import XCTest
+@testable import SwiftMocking
+
+final class AssertTests: XCTestCase {
+
+    var spy: Spy<String, None, Void>!
+
+    override func setUp() {
+        super.setUp()
+        spy = Spy()
+        spy.when(calledWith: .any).thenReturn(())
+    }
+
+    func testAssertCalled() throws {
+        spy.call("test")
+        let assert = Assert(spy: spy)
+        try assert.assert(nil)
+    }
+
+    func testAssertCalledWithMatcher() throws {
+        spy.call("test")
+        let assert = Assert(invocationMatcher: .init(matchers: .equal("test")), spy: spy)
+        try assert.assert(nil)
+    }
+
+    func testAssertCalledFailsWhenNoInvocations() {
+        let assert = Assert(spy: spy)
+        XCTAssertThrowsError(try assert.assert(nil)) {
+            guard let error = $0 as? MockingError else {
+                return XCTFail("Wrong error type")
+            }
+            XCTAssertEqual(error, .unfulfilledCallCount(0))
+        }
+    }
+
+    func testAssertCalledFailsWhenNoMatchingInvocations() {
+        spy.call("other")
+        let assert = Assert(invocationMatcher: .init(matchers: .equal("test")), spy: spy)
+        XCTAssertThrowsError(try assert.assert(nil)) {
+            guard let error = $0 as? MockingError else {
+                return XCTFail("Wrong error type")
+            }
+            XCTAssertEqual(error, .unfulfilledCallCount(0))
+        }
+    }
+
+    func testDoesThrow() throws {
+        let throwingSpy = Spy<String, Throws, Void>()
+        throwingSpy.when(calledWith: .any).thenThrow(TestError.example)
+        _ = try? throwingSpy.call("test")
+
+        let assert = Assert(spy: throwingSpy)
+        try assert.doesThrow()
+    }
+
+    func testDoesThrowFailsWhenNoErrorThrown() {
+        let throwingSpy = Spy<String, Throws, Void>()
+        throwingSpy.when(calledWith: .any).thenReturn(())
+        _ = try? throwingSpy.call("test")
+
+        let assert = Assert(spy: throwingSpy)
+        XCTAssertThrowsError(try assert.doesThrow()) {
+            guard let error = $0 as? MockingError else {
+                return XCTFail("Wrong error type")
+            }
+            XCTAssertEqual(error, .didNotThrow)
+        }
+    }
+
+    func testDoesThrowWithMatcher() throws {
+        let throwingSpy = Spy<String, Throws, Void>()
+        throwingSpy.when(calledWith: .any).thenThrow(TestError.example)
+        _ = try? throwingSpy.call("test")
+
+        let assert = Assert(spy: throwingSpy)
+        try assert.doesThrow(.error(TestError.self))
+    }
+
+    func testDoesThrowWithMatcherFailsWhenWrongError() {
+        let throwingSpy = Spy<String, Throws, Void>()
+        throwingSpy.when(calledWith: .any).thenThrow(AnotherError())
+        _ = try? throwingSpy.call("test")
+
+        let assert = Assert(spy: throwingSpy)
+        XCTAssertThrowsError(try assert.doesThrow(.error(TestError.self))) {
+            guard let error = $0 as? MockingError else {
+                return XCTFail("Wrong error type")
+            }
+            XCTAssertEqual(error, .didNotMatchThrown([AnotherError()]))
+        }
+    }
+}
